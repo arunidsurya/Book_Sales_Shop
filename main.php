8afc1f60d828c9f5ad23fc8f81713e6ec76f1bda
Arun Surendran <arunid.surya@gmail.com>
2023-07-27 13:37:24 +0200

    user input form added

--- main.php
+++ main.php
@@ -7,6 +7,20 @@
 </head>
 <body>
     <h1>Book shop Sales</h1>
+    <form action="" method="post">
+        <label for="customer">customer: </label>
+        <input type="text" name="customer" id="customer">
+
+        <label for="product">Product:</label>
+        <input type="text" name="product" id="product">
+
+        <label for="price">Price:</label>
+        <input type="number" name="price" id="price">
+
+        <input type="submit" value="Filter">
+    </form>
+
+
     
 </body>
 </html>
